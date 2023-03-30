09730e142b152225437c2d6c89528332671ce632
Eriniss <jhs2195@gmail.com>
2023-03-30 14:37:07 +0900

    chater3

---
+++ chapter3/baseball/baseball.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Baseball = () => {
+  return (
+    <>
+      <h1>Hellow, world!</h1>
+    </>
+  )
+}
+
+export default Baseball; // 디폴트로 가져오면 import에 {}을 사용하지 않는다.
+// export const hello = 'hello'; 이런식으로 디폴트를 사용하지 않으면 import { hello } 처럼 {}을 사용 해준다.
