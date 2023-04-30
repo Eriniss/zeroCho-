32ad99e8047587c499af71c46023bb5635de553c
Eriniss <jhs2195@gmail.com>
2023-05-01 08:00:42 +0900

    chapter6

--- test.js
+++
@@ -1,3 +0,0 @@
-const arr = [1, 6, 5, 7, 5, 5, 6, 4, 6, 1, 2];
-
-console.log(arr.slice(0, 6));
--- test/src/App.jsx
+++ test/src/App.jsx
@@ -1,18 +1,28 @@
-import  { useState, useEffect } from 'react';
+import React, { useReducer } from 'react';
 
-const App = () => {
-  const [count, setCount] = useState(0);
+const initialState = { count: 0 };
 
-  useEffect(() => {
-    alert('Clicked!');
-  }, [count]);
-
-  return (
-    <div>
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
-    </div>
-  )
+function reducer(state, action) {
+  switch (action.type) {
+    case 'increment':
+      return { count: state.count + 1 };
+    case 'decrement':
+      return { count: state.count - 1 };
+    default:
+      throw new Error('Unexpected action type');
+  }
 }
 
-export default App;
+function Counter() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <>
+      <p>Count: {state.count}</p>
+      <button onClick={() => dispatch({ type: 'increment' })}>+</button>
+      <button onClick={() => dispatch({ type: 'decrement' })}>-</button>
+    </>
+  );
+}
+
+export default Counter;
